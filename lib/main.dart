ae58d4de87aba542cc9a184f573918f92c50cc78
Shivaraj Patil <shivrajp130@gmail.com>
2021-06-16 12:56:16 +0530

    app icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("I am good"),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        backgroundColor: Colors.blueGrey,
-        body: Center(
-          child: Image(
-            image: NetworkImage(
-                "https://images.dog.ceo/breeds/terrier-westhighland/n02098286_5472.jpg"),
-          ),
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("I am good"),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Image(
+          image: AssetImage('images/damngina2.jpg'),
         ),
       ),
     ),
-  );
+  ));
 }
